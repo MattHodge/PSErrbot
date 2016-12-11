6de04d956e75bd29af5422d02b33d7cfc36e6649
Matthew Hodgkins <matthodge@gmail.com>
2016-12-11 23:11:00 +0100

    test build without pip install

--- build.ps1
+++ build.ps1
@@ -1,6 +1,6 @@
 # python -m pip install --upgrade pip
-virtualenv -p C:\Python35\python.exe errbot
-.\errbot\Scripts\activate.ps1
-python -m pip install --upgrade pip
-pip install -r requirements.txt
+# virtualenv -p C:\Python35\python.exe errbot
+# .\errbot\Scripts\activate.ps1
+# python -m pip install --upgrade pip
+# pip install -r requirements.txt
 & nuget pack -version $($env:APPVEYOR_BUILD_VERSION)
